27821fc578cdaddbe53dfd0bfa6d22c1bd404084
OKAY808 <georgedautov808@gmail.com>
2022-02-28 23:12:57 +0200

    added premake.lua

---
+++ premake5.lua
@@ -0,0 +1,25 @@
+project "Box2D"
+    language "C++"
+    kind "StaticLib"
+    cppdialect "C++11"
+
+    targetdir ("%{prj.location}/bin/" .. outputdir)
+    objdir ("%{prj.location}/bin-int/" .. outputdir)
+
+    files 
+    {
+        "src/**.cpp"
+        "src/**.h"
+        "inlcude/**.h"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+        
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
