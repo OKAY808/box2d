00fbb2c69faf3b9af137d47a6f92f3268e24a331
OKAY <georgedautov808@gmail.com>
2022-03-26 16:52:13 +0200

    added staticruntime

--- premake5.lua
+++ premake5.lua
@@ -2,6 +2,7 @@ project "Box2D"
     language "C++"
     kind "StaticLib"
     cppdialect "C++11"
+    staticruntime "on"
 
     targetdir ("%{prj.location}/bin/" .. outputdir)
     objdir ("%{prj.location}/bin-int/" .. outputdir)
